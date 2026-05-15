5354da6c402d98bee4999dfad0654b8d2b79a2ac
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2026-05-15 20:13:28 -0300

    initialize Spring Boot application with health check endpoint and Maven setup

---
+++ class3/src/test/java/br/com/leaf/class3/Class3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leaf.class3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
